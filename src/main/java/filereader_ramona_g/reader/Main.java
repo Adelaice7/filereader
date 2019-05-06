98de689311216784faa9bd762a210f72014469da
Adelaice7 <gotthardramona@gmail.com>
2019-05-06 14:07:45 +0200

    Formatting code

--- src/main/java/filereader_ramona_g/reader/Main.java
+++ src/main/java/filereader_ramona_g/reader/Main.java
@@ -9,66 +9,62 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
-	
-	public static void printFileToConsole(String[] args) {
-		String input_path = args[0];
-		File inputFile = new File(input_path);
-		String line = null;
-		
-		try (
-				BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-			) {
-			
-			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
-				
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Wrong input file path given!");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void copyFileToFile(String[] args) {
-		String input_path = args[0];
-		File inputFile = new File(input_path);
-		String output_path = args[1];
-		File outputFile = new File(output_path);
-		String line = null;
-		
-		try (
-				BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-				BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile, false));
-			) {
-			
-			while ((line = reader.readLine()) != null) {
-				if (args.length > 1) {
-					writer.write(line);
-					writer.newLine();
-					
-				} else {
-					System.out.println(line);
-				}
-			}
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
 
-	public static void main(String[] args) {
-		
-		if (args.length > 0) {
-			printFileToConsole(args);
-			
-			if (args.length > 1) {
-				copyFileToFile(args);
-			}			
-			
-		} else {
-			System.out.println("No parameters entered.");
-		}
-	}
+    public static void printFileToConsole(String[] args) {
+        String input_path = args[0];
+        File inputFile = new File(input_path);
+        String line = null;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));) {
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Wrong input file path given!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void copyFileToFile(String[] args) {
+        String input_path = args[0];
+        File inputFile = new File(input_path);
+        String output_path = args[1];
+        File outputFile = new File(output_path);
+        String line = null;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile, false));) {
+
+            while ((line = reader.readLine()) != null) {
+                if (args.length > 1) {
+                    writer.write(line);
+                    writer.newLine();
+
+                } else {
+                    System.out.println(line);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            printFileToConsole(args);
+
+            if (args.length > 1) {
+                copyFileToFile(args);
+            }
+
+        } else {
+            System.out.println("No parameters entered.");
+        }
+    }
 }
--- src/test/java/filereader_ramona_g/reader/TestApp.java
+++ src/test/java/filereader_ramona_g/reader/TestApp.java
@@ -13,52 +13,48 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestApp {
-	
-	@Test
-	@DisplayName("Test Print To Console Function")
-	public void testMainPrintToConsole() throws IOException {
-		String inpath = "C:\\Users\\gotthard\\Documents\\valami.txt";
-		
-		ByteArrayOutputStream os = new  ByteArrayOutputStream();
-		PrintStream testStream = new PrintStream(os);
-		System.setOut(testStream);
 
-		File inputFile = new File(inpath);
+    @Test
+    @DisplayName("Test Print To Console Function")
+    public void testMainPrintToConsole() throws IOException {
+        String inpath = "C:\\Users\\gotthard\\Documents\\valami.txt";
 
-		String[] args = new String[1];
-		args[0] = inpath;
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        PrintStream testStream = new PrintStream(os);
+        System.setOut(testStream);
 
-		Main.main(args);
-		
-		try (
-				BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-				ByteArrayInputStream in = new ByteArrayInputStream(os.toByteArray());
-			) {
+        File inputFile = new File(inpath);
 
-			Assertions.assertEquals(reader.read(), in.read());
-		}
-	}
-	
-	@Test
-	@DisplayName("Test Copy To Another File Function")
-	public void testMainCopyToFile() throws IOException {
-		String inpath = "C:\\Users\\gotthard\\Documents\\valami.txt";
-		String outpath = "C:\\Users\\gotthard\\Documents\\valami2.txt";
-		File inputFile = new File(inpath);
-		File outFile = new File(outpath);
+        String[] args = new String[1];
+        args[0] = inpath;
 
-		String[] args = new String[2];
-		args[0] = inpath;
-		args[1] = outpath;
-		
-		Main.main(args);
-		
-		try(
-				BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-				BufferedReader testReader = new BufferedReader(new FileReader(outFile));
-				) {
-			
-			Assertions.assertEquals(reader.read(), testReader.read());
-		}
-	}
+        Main.main(args);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+                ByteArrayInputStream in = new ByteArrayInputStream(os.toByteArray());) {
+
+            Assertions.assertEquals(reader.read(), in.read());
+        }
+    }
+
+    @Test
+    @DisplayName("Test Copy To Another File Function")
+    public void testMainCopyToFile() throws IOException {
+        String inpath = "C:\\Users\\gotthard\\Documents\\valami.txt";
+        String outpath = "C:\\Users\\gotthard\\Documents\\valami2.txt";
+        File inputFile = new File(inpath);
+        File outFile = new File(outpath);
+
+        String[] args = new String[2];
+        args[0] = inpath;
+        args[1] = outpath;
+
+        Main.main(args);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+                BufferedReader testReader = new BufferedReader(new FileReader(outFile));) {
+
+            Assertions.assertEquals(reader.read(), testReader.read());
+        }
+    }
 }
