e3d2dd5f87805b62e07044ff313b8024323f6b86
Adelaice7 <gotthardramona@gmail.com>
2019-05-10 10:10:21 +0200

    Fixed variables, functionality

--- src/main/java/filereader_ramona_g/reader/Main.java
+++ src/main/java/filereader_ramona_g/reader/Main.java
@@ -11,8 +11,8 @@ import java.io.IOException;
 public class Main {
 
     public static void printFileToConsole(String[] args) {
-        String input_path = args[0];
-        File inputFile = new File(input_path);
+        String inputPath = args[0];
+        File inputFile = new File(inputPath);
         String line = null;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));) {
@@ -29,10 +29,10 @@ public class Main {
     }
 
     public static void copyFileToFile(String[] args) {
-        String input_path = args[0];
-        File inputFile = new File(input_path);
-        String output_path = args[1];
-        File outputFile = new File(output_path);
+        String inputPath = args[0];
+        File inputFile = new File(inputPath);
+        String outputPath = args[1];
+        File outputFile = new File(outputPath);
         String line = null;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
@@ -57,12 +57,17 @@ public class Main {
     public static void main(String[] args) {
 
         if (args.length > 0) {
-            printFileToConsole(args);
-
-            if (args.length > 1) {
-                copyFileToFile(args);
+            switch (args.length) {
+                case 1:
+                    printFileToConsole(args);
+                    break;
+                   
+                case 2:
+                if (args.length > 1) {
+                    copyFileToFile(args);
+                }
             }
-
+            
         } else {
             System.out.println("No parameters entered.");
         }
